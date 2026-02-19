c440746442d0c4860d00fdf0dae3a1bd51a5cff0
shivaprasad0356 <shivaprasadkorakanchi0972@gmail.com>
2026-02-19 17:16:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('<h1>PIP Week 3: Automation Active</h1><p>Deployment Successful.</p>'));
+app.get('/', (req, res) => res.send('<h1>Week 3: Automation Active</h1><p>Deployment Successful.</p>'));
 app.listen(3000, () => console.log('Server running on port 3000'));
