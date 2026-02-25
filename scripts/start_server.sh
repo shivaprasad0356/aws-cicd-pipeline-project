b0d1b49d39719daab890e67412b4357b06cb4c6d
shivaprasad0356 <shivaprasadkorakanchi0972@gmail.com>
2026-02-25 11:35:23 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,12 +2,12 @@
 # 1. Navigate to the app directory
 cd /var/www/html/app
 
-# 2. Use full paths to ensure the script finds npm and pm2
+# 2. finds npm and pm2
 # Note: On Amazon Linux 2023, these are usually in /usr/bin/
 /usr/bin/npm install
 
 # 3. Reload or Start the application
-# We use 'reload' for zero-downtime updates if the app is already up
+# 'reload' 
 if /usr/bin/pm2 list | grep -q "my-pro-app"; then
     echo "Reloading existing application..."
     /usr/bin/pm2 reload "my-pro-app"
